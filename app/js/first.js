28e72159d1f7ac722face7d872a6eac8d57b81be
Karl <karl.l1@hotmail.com>
2017-02-10 13:56:43 +1100

    highlight text

--- app/js/first.js
+++ app/js/first.js
@@ -4,11 +4,15 @@ $(document).ready(function(){
 
  $(function(){
       $(".element").typed({
-        strings: ["web developers.", "graphic designers.","solution architects."],
+        strings: ["web developers.", "graphic designers.","solution architects.", "interface engineers.", ],
         typeSpeed: 100,
         loop: true,
         showCursor: true,
         backDelay: 800,
-        startDelay: 500
+        startDelay: 1000
       });
   });
+
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });  
